e181feca8d8afb22469f5db314217a8010b82512
yuzelli <zsl5155@qq.com>
2017-08-08 22:39:02 +0800

    updata project catalog

---
+++ app/src/main/java/com/example/yuzelli/carplaterecognition/constants/ConstantsUtils.java
@@ -0,0 +1,23 @@
+package com.example.yuzelli.carplaterecognition.constants;
+
+/**
+ * Created by 51644 on 2017/7/5.
+ */
+
+public class ConstantsUtils {
+
+    public static final int SPLASH_START_ACTIVITY = 0x00001001;
+
+
+    //保持登录用户信息
+    public static final String SP_LOGIN_USER_INFO = "UserInfo";
+    public static final String SP_MU_BIAO_DEVICE_INFO = "MUBIAOSHEBEI";
+    public static final String SP_TIME = "TIME_SETING";
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/yuzelli/carplaterecognition/view/activity/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.yuzelli.carplaterecognition.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yuzelli.carplaterecognition.R;
+import com.example.yuzelli.carplaterecognition.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected int layoutInit() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void binEvent() {
+
+    }
+
+    @Override
+    protected void fillData() {
+
+    }
+}
